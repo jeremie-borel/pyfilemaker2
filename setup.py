f01b17fb599059b97a354db149f0315f8f7707d8
Jérémie <jeremie.borel@vd.educanet2.ch>
2019-07-08 13:07:36 +0200

    version 0.1.14

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyfilemaker2',
-    version="0.1.12.4",
+    version="0.1.14",
     description='Python Object Wrapper for FileMaker Server XML Interface',
     long_description=long_description,
-    # long_description_content_type="text/markdown",
+    long_description_content_type="text/markdown",
     classifiers=[
         'Environment :: Console',
         'Intended Audience :: Developers',
