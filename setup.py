b011fdfbe9d017b948098b5623cc64f3a8b19266
Jérémie <jeremie.borel@eduvaud.ch>
2021-09-28 14:26:18 +0200

    some doc change in pypi packaging...

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='pyfilemaker2',
-    version="0.2.1",
+    version="0.2.2",
     description='Python Object Wrapper for FileMaker Server XML Interface',
     long_description=long_description,
     long_description_content_type="text/markdown",
